27e68d323405fefb95579912e7e1fb04789dfedd
Emmanuel Adesile <emmanueladesile@Emmanuels-MacBook-Pro.local>
2020-05-06 15:27:53 +0100

    update readme file reduce the height of the readme

--- src/components/header.js
+++ src/components/header.js
@@ -4,8 +4,8 @@ import { colors } from "../styles/theme";
 import bgHeader from "../static/images/bg-header-desktop.svg";
 
 const StyledHeader = styled.header`
-  height: 200px;
-  background-color: ${colors.darkGreyCyan};
+  height: 150px;
+  background-color: ${colors.primary};
   background-image: url(${bgHeader});
   background-size: cover;
 
